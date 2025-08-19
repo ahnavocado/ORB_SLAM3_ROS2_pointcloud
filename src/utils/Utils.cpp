057d2960e1787221721a33813f677b92c66acc43
ahnavocado <ahn.avocado@gmail.com>
2025-08-19 23:21:20 +0900

    FIX : Fixed ROS type codes of Utils.cpp Utils.h

--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -1,18 +1,30 @@
 //
 // Created by vuong on 16/12/2021.
 //
+#include <geometry_msgs/msg/transform.hpp>
+#include <rclcpp/rclcpp.hpp>
 
 #include "utils/Utils.h"
-void Utils::toTransformMsg(Sophus::SE3f Twc, eometry_msgs::msg::Transform* tf) {
-  tf->translation.x = Twc.translation().x();
-  tf->translation.y = Twc.translation().y();
-  tf->translation.z = Twc.translation().z();
-  tf->rotation.w = Twc.unit_quaternion().w();
-  tf->rotation.x = Twc.unit_quaternion().x();
-  tf->rotation.y = Twc.unit_quaternion().y();
-  tf->rotation.z = Twc.unit_quaternion().z();
+namespace Utils {
+
+void toTransformMsg(const Sophus::SE3f& Twc, geometry_msgs::msg::Transform* tf)
+{
+  // translation
+  tf->translation.x = static_cast<double>(Twc.translation().x());
+  tf->translation.y = static_cast<double>(Twc.translation().y());
+  tf->translation.z = static_cast<double>(Twc.translation().z());
+
+  // rotation (Sophus -> Eigen quaternion)
+  const auto q = Twc.unit_quaternion();
+  tf->rotation.w = static_cast<double>(q.w());
+  tf->rotation.x = static_cast<double>(q.x());
+  tf->rotation.y = static_cast<double>(q.y());
+  tf->rotation.z = static_cast<double>(q.z());
 }
-rclcpp::Time Utils::toROSTime(double timestamp) {
-  rclcpp::Time output(static_cast<int64_t>(timestamp * 1e9));
-  return output;
+
+rclcpp::Time toROSTime(double timestamp)
+{
+  // 초 → 나노초 정수
+  const int64_t nsec = static_cast<int64_t>(timestamp * 1e9);
+  return rclcpp::Time(nsec);
 }
